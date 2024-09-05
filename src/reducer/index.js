2971790d10494208b6c9872b8feb3b2bd5083a4a
DaniloWaffis <danilowaffis@gmail.com>
2024-09-05 14:21:18 +0100

    use  shorthand syntax for a better code

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import user from "./user";
 import contacts from "./contactjs";
+import activeUserId from "./activeUserId";
 
 export default combineReducers({
-  user: user,
-  contacts: contacts,
+  user,
+  contacts,
+  activeUserId,
 });
