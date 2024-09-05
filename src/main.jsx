ce8779b91ea0834895d13ed72f62e1d72318a6fe
DaniloWaffis <danilowaffis@gmail.com>
2024-09-05 13:29:15 +0100

    a root component

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./containers/App.jsx";
+import "./index.css";
+import store from "./store/index.js";
+
+const root = createRoot(document.getElementById("root"));
+function renderApp() {
+  fancyLog();
+  root.render(
+    <>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </>
+  );
+}
+function fancyLog() {
+  console.log("%c Rendered with👌👍", "background: purple; color: #fff");
+  console.log(store.getState());
+}
+renderApp();
+store.subscribe(renderApp);
