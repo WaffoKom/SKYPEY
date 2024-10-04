36a348d4466176cd9f19590bb9a2077fcd424f49
DaniloWaffis <danilowaffis@gmail.com>
2024-10-04 15:01:24 +0100

    add user and  activeUserId to the state application

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Sidebar from "../components/Sidebar.jsx";
 import Origin from "../Origin.jsx";
-import "./App.css";
 import store from "../store/index.js";
 import _ from "lodash";
+import "./App.css";
 
 export default function App() {
-  const { contacts } = store.getState();
+  const { contacts, user, activeUserId } = store.getState();
   return (
     <div className="App">
       <Sidebar contacts={_.values(contacts)} />
-      <Origin />
+      <Origin user={user} activeUserId={activeUserId} />
     </div>
   );
 }
