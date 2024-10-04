9904f785ac64ce9ab7bbc9b789a03441bdcbeddf
DaniloWaffis <danilowaffis@gmail.com>
2024-10-04 15:37:57 +0100

    Empty component

--- src/components/Empty.jsx
+++ src/components/Empty.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 import "./Empty.css";
-export default function Empty() {
-  return <div>Empty</div>;
+export default function Empty({ user }) {
+  const { name, profile_pic, status } = user;
+  const first_name = name.split(" ")[0];
+  return (
+    <div className="Empty">
+      <h1 className="Empty__name">Welcome, {first_name} </h1>
+      <img src={profile_pic} alt={name} className="Empty__img" />
+      <p className="Empty__status">
+        <b>Status:</b> {status}
+      </p>
+      <button className="Empty__btn">Start a conversation</button>
+      <p className="Empty__info">
+        Search for someone to start chatting with or go to Contacts to see who
+        is available
+      </p>
+    </div>
+  );
 }
