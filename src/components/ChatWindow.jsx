cc712128327ac28636fc29766b8a3c77ada17f49
DaniloWaffis <danilowaffis@gmail.com>
2024-10-04 15:38:45 +0100

    ChatWindow  pass activeUserId prop

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./ChatWindow.css";
 
-export default function ChatWindow() {
-  return <div>ChatWindow</div>;
+export default function ChatWindow({ activeUserId }) {
+  return (
+    <div className="ChatWindow">
+      Conversation for user id:
+      {activeUserId}
+    </div>
+  );
 }
