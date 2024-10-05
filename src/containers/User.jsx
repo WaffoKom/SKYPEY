bf566895f09e1787ac05b564eab9ef64b00b20c9
DaniloWaffis <danilowaffis@gmail.com>
2024-10-05 14:40:50 +0100

    dispatch action in User component

--- src/containers/User.jsx
+++ src/containers/User.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./User.css";
+import store from "../store/index.js";
+import { setACtiveUserId } from "../actions/index.js";
 
 export default function User({ user }) {
+  function handleClickUser({ user_id }) {
+    console.log(user_id);
+
+    store.dispatch(setACtiveUserId(user_id));
+  }
   const { name, profile_pic, status } = user;
   return (
-    <div className="User">
+    <div className="User" onClick={handleClickUser.bind(null, user)}>
       <img src={profile_pic} alt={name} className="User_pic" />
       <div className="User_details">
         <p className="User_details_name">{name}</p>
