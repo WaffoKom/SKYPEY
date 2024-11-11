473937056df3aa1086b3cea51c86561aa610b1d3
DaniloWaffis <danilowaffis@gmail.com>
2024-11-11 12:30:49 +0100

    correct the name file

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
-import user from "./user";
-import contacts from "./contactjs";
-import activeUserId from "./activeUserId";
+import user from "./user.js";
+import contacts from "./contacts.js";
+import activeUserId from "./activeUserId.js";
+import messages from "./messages.js";
 
 export default combineReducers({
   user,
   contacts,
   activeUserId,
+  messages,
 });
