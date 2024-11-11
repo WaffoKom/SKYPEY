5f0417eef9b5731ca0aa3fd95d475b91df4806db
DaniloWaffis <danilowaffis@gmail.com>
2024-11-11 12:30:00 +0100

    implemention of the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./Header.css";
 
-export default function Header() {
-  return <div>Header</div>;
+export default function Header({ user }) {
+  const { name, status } = user;
+  return (
+    <header className="Header">
+      <h1 className="Header__name">{name}</h1>
+      <p className="Header__status">{status}</p>
+    </header>
+  );
 }
