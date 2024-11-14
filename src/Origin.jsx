785266441f20ccc60ef3021d14a095c224b6f1a0
DaniloWaffis <danilowaffis@gmail.com>
2024-11-14 11:48:13 +0100

    correct path

--- src/Origin.jsx
+++ src/Origin.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Origin.css";
 import Empty from "./components/Empty.jsx";
-import ChatWindow from "./components/ChatWindow.jsx";
+import ChatWindow from "./containers/ChatWindow.jsx";
 export default function Origin({ user, activeUserId }) {
   const renderMainContent = () => {
     if (!activeUserId) {
