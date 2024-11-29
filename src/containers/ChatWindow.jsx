a7a381be92f25354bc111431cf498f9206483e65
DaniloWaffis <danilowaffis@gmail.com>
2024-11-29 23:01:45 +0100

    The rest

--- src/containers/ChatWindow.jsx
+++ src/containers/ChatWindow.jsx
@@ -1,18 +1,25 @@
-import React from "react";
 import "./ChatWindow.css";
 import store from "../store/index.js";
 import Header from "../components/Header.jsx";
 import Chats from "../components/Chats.jsx";
+import MessageInput from "./MessageInput.jsx";
 import _ from "lodash";
 
 export default function ChatWindow({ activeUserId }) {
   const state = store.getState();
+  const { typing } = state;
   const activeUser = state.contacts[activeUserId];
   const activeMsgs = state.messages[activeUserId];
+
   return (
     <div className="ChatWindow">
       <Header user={activeUser} />
-      <Chats messages={_.values(activeMsgs)} />
+      <Chats
+        messages={_.values(activeMsgs).filter((m) => {
+          return m.text.length !== 0;
+        })}
+      />
+      <MessageInput value={typing} />
     </div>
   );
 }
