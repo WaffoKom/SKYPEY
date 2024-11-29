3468419703dfc487f221b0ff97aa9631e36cfdb1
DaniloWaffis <danilowaffis@gmail.com>
2024-11-29 22:59:03 +0100

    action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,23 @@
-import { SET_ACTIVE_USER_ID } from "../constants/action-types.js";
+import {
+  SET_ACTIVE_USER_ID,
+  SET_TYPING_VALUE,
+  SEND_MESSAGE,
+} from "../constants/action-types.js";
 
 export const setACtiveUserId = (user_id) => ({
   type: SET_ACTIVE_USER_ID,
   payload: user_id,
+});
+
+export const setTypingValue = (value) => ({
+  type: SET_TYPING_VALUE,
+  payload: value,
+});
+
+export const sendMessage = (message, userId) => ({
+  type: SEND_MESSAGE,
+  payload: {
+    message,
+    userId,
+  },
 });
